29aeb849066c1b12c34d2153f26ebac20687ac07
Phil Johnston <support@mintplugins.com>
2015-09-17 12:22:01 -0400

    Admin Meta Scripts now enqueued only when needed.

---
+++ includes/misc-functions/admin-enqueue-scripts.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * MP Stacks + EddCart Enqueue Scripts
+ *
+ * @link http://mintplugins.com/doc/
+ * @since 1.0.0
+ *
+ * @package    MP Stacks + EddCart
+ * @subpackage Functions
+ *
+ * @copyright   Copyright (c) 2015, Mint Plugins
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @author      Philip Johnston
+ */
